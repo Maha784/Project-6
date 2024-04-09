cc0a5fce1ec7fee121fa2201ce6a9781aa29eafc
Maha784 <158258797+Maha784@users.noreply.github.com>
2024-04-09 23:32:55 +0300

    Delete PROJECT/backend directory

--- PROJECT/backend/user_feedback.py
+++
@@ -1,6 +0,0 @@
-class UserFeedback:
-    def __init__(self):
-        self.feedback = []
-
-    def submit_feedback(self, user_id, feedback_text):
-        self.feedback.append({"user_id": user_id, "feedback": feedback_text})
--- PROJECT/backend/wellness_resource.py
+++
@@ -1,6 +0,0 @@
-class WellnessResources:
-    def __init__(self):
-        self.articles = []
-
-    def add_article(self, title, content):
-        self.articles.append({"title": title, "content": content})
